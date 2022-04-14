f105ce1621384dc94af6ffb7f22005fad90bfd58
crisepu <csepulvedavenegas@gmail.com>
2022-04-14 19:04:52 +0000

    fix navbar

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,39 +3,54 @@ import PropType from "prop-types";
 
 const Navbar = (props) => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+		<nav className="navbar navbar-expand-lg navbar-light bg-dark px-3">
 			<div className="container-fluid">
-				<a className="navbar-brand text-light" href="#">
-					Bootstrap with React
+				<a className="navbar-brand text-white" href="#">
+					Start Bootcamp
 				</a>
 				<button
 					className="navbar-toggler"
 					type="button"
 					data-bs-toggle="collapse"
-					data-bs-target="#navbarNavAltMarkup"
-					aria-controls="navbarNavAltMarkup"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
 					aria-expanded="false"
 					aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div
-					className="collapse navbar-collapse d-flex flex-row-reverse bd-highlight"
-					id="navbarNavAltMarkup">
-					<div className="navbar-nav">
-						<a
-							className="nav-link text-light"
-							aria-current="page"
-							href="#">
-							Home
-						</a>
-						<a className="nav-link text-muted" href="#">
-							Features
-						</a>
-						<a className="nav-link text-muted" href="#">
-							Pricing
-						</a>
-						<a className="nav-link disabled text-muted">Disabled</a>
-					</div>
+					className="collapse navbar-collapse "
+					id="navbarSupportedContent">
+					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a
+								className="nav-link active text-white"
+								aria-current="page"
+								href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-muted" href="#">
+								About
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-muted" href="#">
+								Services
+							</a>
+						</li>
+
+						<li className="nav-item">
+							<a
+								className="nav-link disabled text-muted"
+								href="#"
+								tabindex="-1"
+								aria-disabled="true">
+								Contact
+							</a>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</nav>
